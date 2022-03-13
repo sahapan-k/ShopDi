cccacd240138946be9924de74f36eca1eb822686
sahapan-k <sahapan.kanjanothai@gmail.com>
2022-03-13 07:38:54 +0700

    fix files

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ const app = require('./app');
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 dotenv.config({ path: './config.env' });
